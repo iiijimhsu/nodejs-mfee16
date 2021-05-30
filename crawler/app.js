2d0d8a2bf142e9978488f56ce32878259435bf21
jimhsu <iiijimhsu@gmail.com>
2021-05-30 10:59:39 +0800

    app.js

--- crawler/app.js
+++ crawler/app.js
@@ -5,13 +5,13 @@
 
 //引入axios
 
-const axios = require('axios');
+const axios = require("axios");
 
-axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210523&stockNo=3661')
+axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210523&stockNo=2610")
   .then(function (response) {
     // handle success
     console.log(response);
-    if(response.data.stat === 'OK') {
+    if(response.data.stat === "OK") {
         console.log(response.data.date);
         console.log(response.data.title);
         
