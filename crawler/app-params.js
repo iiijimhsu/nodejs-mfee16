54244d48d0954d877a0af74dbf89846242851128
jimhsu <iiijimhsu@gmail.com>
2021-06-02 23:51:13 +0800

    homework

---
+++ crawler/app-params.js
@@ -0,0 +1,23 @@
+https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210524&stockNo=2002
+
+// response=json   //格式
+// &date=20210524  //日期
+// &stockNo=2002   // 股票代號
+var axios = require('axios');
+
+axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?', {
+    params: {
+        response:'json',
+        date:'20210524',
+        stockNo:'3661',
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });  
