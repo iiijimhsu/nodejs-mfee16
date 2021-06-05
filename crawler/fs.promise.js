8cbe7cbc1eba692d67e59bd448fa742fcdca8444
iiijimhsu <83852381+iiijimhsu@users.noreply.github.com>
2021-06-05 10:58:19 +0800

    Create fs.promise.js

---
+++ crawler/fs.promise.js
@@ -0,0 +1,45 @@
+// https://www.twse.com.tw/exchangeReport/STOCK_DAY
+// ?response=json
+// &date=20210523
+// &stockNo=2610
+
+const axios = require("axios");
+//引入 promise fs
+const fs = require("fs/promises");
+const moment = require("moment");
+
+// console.log(moment().format()); // 2021-05-30T13:45:06+08:00
+// console.log(moment().format("YYYYMMDD")); // 20210530
+
+// function readFilePromise() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("stock.txt", "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// }
+
+fs.readFile("stock.txt", "utf8")
+  .then((stockCode) => {
+    console.log("stockCode:", stockCode);
+
+    return axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
+      params: {
+        response: "json",
+        date: moment().format("YYYYMMDD"),
+        stockNo: stockCode,
+      },
+    });
+  })
+  .then((response) => {
+    if (response.data.stat === "OK") {
+      console.log(response.data.date);
+      console.log(response.data.title);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
