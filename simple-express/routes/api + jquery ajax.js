ba65c315f0b719ef9f0d06c43262c239e4fa6141
iiijimhsu <83852381+iiijimhsu@users.noreply.github.com>
2021-06-20 04:20:38 +0800

    Update api + jquery ajax.js

--- simple-express/routes/api + jquery ajax.js
+++ simple-express/routes/api + jquery ajax.js
@@ -1,10 +1,30 @@
 // TODO jquery.ajax 去取得 api/stocks 的資料
-
 $(function () {
-    $.ajax({
-            type:"GET",
-            url:"/stocks",
-        }).done(function(data){
-            console.log(data);
-        });
-        });
+// $.ajax({
+//     type: "GET",
+//     url: "/api/stocks",
+// }).done(function (data) {
+//     console.log(data)
+// });
+
+//axios.get('/api/stocks')
+//.then(function (response) {
+//  console.log(response.data);
+//})
+// .catch(function (error) {
+//     console.log(error);
+// })
+// .finally(function () {
+
+// });
+    
+//* fetch -> promised-based */
+// then or async / await
+fetch('/api/stock')
+.then(function (response) {
+ return response.json();
+ })
+  .then(function (data) {
+  console.log(data);
+ });
+});
