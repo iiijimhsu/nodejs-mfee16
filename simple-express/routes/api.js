8022ed140b9b4c2ce912489234e5582719e78259
iiijimhsu <83852381+iiijimhsu@users.noreply.github.com>
2021-06-27 03:01:40 +0800

    6/27

--- simple-express/routes/api.js
+++ simple-express/routes/api.js
@@ -2,17 +2,10 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../utils/db");
 
-router.get("/stocks",async (req, res)=>{
-    let queryResults = await connection.queryAsync("SELECT * FROM stock;");
-    res.json(queryResults);
+router.get("/stocks", async (req, res) => {
+  let queryResults = await connection.queryAsync("SELECT * FROM stock;");
 
-})
-router.get("/stocks/:stockCode", async (req, res)=>{
-    let stockdetail = await connection.queryAsync(
-			"SELECT * FROM stock_price WHERE stock_id = ? ORDER BY date;",
-			req.params.stockCode
-		);
-        res.json(stockdetail)
-})
+  res.json(queryResults);
+});
 
 module.exports = router;
